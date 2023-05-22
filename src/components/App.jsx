7983727b4f9c13580530bab1fcc0ffc2d02fd75c
alionash <112432218+alionash@users.noreply.github.com>
2023-05-22 22:49:31 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { ContactsForm } from "./ContactsForm/contactsForm";
+import ContactList from "./ContactsList/contactsList";
+import Filter from "./Filter/filter";
+import { Section, Title, SectionHeading } from "./GlobalStyles.styled";
+
+export class App extends Component{
+
+state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+}
+
+  addNewContact = (newContact, { actions }) => {
+    const { contacts } = this.state;
+    const existName = contacts.map(el => el.name.toLowerCase());
+
+    return existName.includes(newContact.name.toLowerCase()) ?
+      alert(`${newContact.name} is already in contacts`):
+      this.setState(prevState => {
+        actions.resetForm();
+        return {
+          contacts: [...prevState.contacts, newContact]
+        }
+      
+      })
+    
+  }
+
+  getVisibleContact = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizeFilter = filter.toLocaleLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  delContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(item => item.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  render() {
+    const getVisibleContact = this.getVisibleContact();
+    
+    return (
+      <Section>
+        <Title>Phonebook</Title>
+        <ContactsForm
+          onSubmit={ this.addNewContact } />
+      
+        <SectionHeading>Contacts</SectionHeading>
+        <Filter filter={this.state.filter} onChange={this.changeFilter} />
+        <ContactList contacts={getVisibleContact}
+          onDelete={ this.delContact } />
+    </Section>
+    )
+  }
+}
+
+
