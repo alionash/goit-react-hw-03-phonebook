4771b370bdeda1df3f129881e14378688b48c71a
alionash <112432218+alionash@users.noreply.github.com>
2023-05-23 15:43:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,37 @@ import { Section, Title, SectionHeading } from "./GlobalStyles.styled";
 export class App extends Component{
 
 state = {
-  contacts: [
+  contacts: [],
+  filter: '',
+}
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    } else {
+      this.setState({
+        contacts: [
     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-  filter: '',
-}
+  ]
+      })
+    }
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate')
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+      
+    }
+  }
 
   addNewContact = (newContact, { actions }) => {
     const { contacts } = this.state;
